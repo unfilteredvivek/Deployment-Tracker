68af052bbf2fafda41e2d0a037aa486fe58ed90a
vivek singh <unfilteredvivek@gmail.com>
2026-04-20 12:53:06 +0530

    v3 update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const VERSION = "v2";
+const VERSION = "v3";
 
 app.get("/", (req, res) => {
   res.send(`App is running 🚀 | Version: ${VERSION}`);
