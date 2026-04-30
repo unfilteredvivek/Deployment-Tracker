3d2488be1007598011cf9b36032248f9b91c2311
vivek singh <unfilteredvivek@gmail.com>
2026-04-30 17:45:19 +0530

    fixed app.js

--- app.js
+++ app.js
@@ -27,14 +27,14 @@ docker run -d -p 3001:3000 --name deployed-app unfilteredvivek/deployment-tracke
 `;
 
   exec(cmd, (err, stdout, stderr) => {
-  if (err) {
-    console.log("ERROR:", err);
-    console.log("STDERR:", stderr);
-    return res.send(`Deployment failed ❌ \n ${stderr}`);
-  }
-  res.send(`Deployed version: ${version} ✅`);
+    if (err) {
+     console.log("ERROR:", err);
+     console.log("STDERR:", stderr);
+     return res.send(`Deployment failed ❌ \n ${stderr}`);
+   }
+   res.send(`Deployed version: ${version} ✅`);
+ });
 });
-
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
